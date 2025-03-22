03ff29ba3cb74abefd70c02a2a0fa315195400ef
Fabio <mastromaurofabio@gmail.com>
2025-03-22 19:35:00 +0100

    Aggiornato il metodo add, adesso carica anche il contenuto della bag, aggiunte la data per la bag e aggiunta la data anche alla database.db

--- WA_PROJECT_DB.mjs
+++ WA_PROJECT_DB.mjs
@@ -6,21 +6,20 @@ import dayjs from 'dayjs';
 const db = new sqlite.Database('database.sqlite', (err) => { if (err) throw err });
 
 
-function Bag(bagid, size, type, price, establishment, status, content = []) {
+function Bag(bagid, size, type, price, establishment, status = 'Available', content = []) {
     this.bagid = bagid;
     this.size = size;
     this.type = type;
     this.price = price;
     this.establishment = establishment;
+    this.status = status;
+    this.day = dayjs().format("DD/MM/YYYY HH:mm:ss");
     this.status = "Available";
     //The content if regular takes the inpunt content else empty
     this.content = type == "Regular" ? content : [];
 
 }
-//TODO aggiungere il tempo (per le prenotazioni)
-//aggiungere le bag surprise che non permettono di vedere il contenuto
-//aggiungere la possibilità di prendere il contenuto delle bag (se reguar)
-//
+
 
 //data of resturant/store with the list of bags
 function Establishment(rsid, name, address, phone, category) {
@@ -33,13 +32,34 @@ function Establishment(rsid, name, address, phone, category) {
 
     //add a bag of the resturant
     this.add = (bag) => new Promise((resolve, reject) => {
-        const sql = 'INSERT INTO bags (size, type, price, establishment_id) VALUES (?,?,?,?) returning bagid';
+        let array = [];
+        let sql = 'INSERT INTO bags (size, type, price, establishment_id, date) VALUES (?,?,?,?,?) returning bagid';
 
-        db.get(sql, [bag.size, bag.type, bag.price, this.rsid, bag.status], (err, row) => {
+        db.get(sql, [bag.size, bag.type, bag.price, this.rsid, bag.day, ...array], (err, row) => {
             if (err)
                 reject(err);
-            else
-                resolve(row);
+            else {
+
+                sql = 'INSERT INTO bag_content(bag_id, item_name, quantity) VALUES';
+                for (const con of bag.content) {
+                    sql += ' (?,?,?),';
+                    array.push(row['bagid'], con.name, con.quantity);
+                }
+                //removing the last comma ","
+                sql = sql.slice(0, -1);
+
+                db.run(sql, [...array], (err) => {
+                    if (err)
+                        reject(err);
+                    else {
+                        resolve(row);
+                    }
+
+                })
+
+            }
+
+
         })
     })
 
@@ -64,6 +84,7 @@ function Establishment(rsid, name, address, phone, category) {
                 resolve("Added")
         })
     })
+
     //Remove an element from a BAG
     this.removeElement = (id) => new Promise((resolve, reject) => {
         const sql = 'DELETE FROM bag_content WHERE id = ?'
@@ -96,83 +117,67 @@ function ListOfEstablishments() {
 
 
     // Sorting method by alphabetical order
-    this.sortByName = () => {return new Promise((resolve, reject)=>{
-        const sql = 'SELECT * FROM establishments Order by name ASC';
+    this.sortByName = () => {
+        return new Promise((resolve, reject) => {
+            const sql = 'SELECT * FROM establishments Order by name ASC';
 
-        db.all(sql, [], (err, rows)=>{
-            if(err)
-                reject(err);
-            else{
-                const result = rows.map((row) =>new Establishment(row.rsid, row.name, row.address, row.phone, row.category));
-                resolve(result);
-            }
+            db.all(sql, [], (err, rows) => {
+                if (err)
+                    reject(err);
+                else {
+                    const result = rows.map((row) => new Establishment(row.rsid, row.name, row.address, row.phone, row.category));
+                    resolve(result);
+                }
             })
-    })}
+        })
+    }
 
-//filter for all the needed parameters
-    this.findBy = (arrayParams) => new Promise((resolve, reject)=>{
-       let sql = `SELECT * 
+    //filter for all the needed parameters
+    this.findBy = (arrayParams) => new Promise((resolve, reject) => {
+        let sql = `SELECT * 
        FROM establishments as e, bags as b, bag_content as bc
        WHERE 1=1 AND e.rsid = b.establishment_id 
        AND bc.bag_id = b.bagid`
-       
-       let params = [];
 
-        if(arrayParams.name){
+        let params = [];
+
+        if (arrayParams.name) {
             sql += " AND name = ? ";
             params.push(arrayParams.name);
         }
-        if(arrayParams.price){
+        if (arrayParams.price) {
             sql += " AND price <= ? ";
             params.push(arrayParams.price);
         }
-        if(arrayParams.type){
+        if (arrayParams.type) {
             sql += " AND type = ? ";
             params.push(arrayParams.type);
         }
-        if(arrayParams.size){
+        if (arrayParams.size) {
             sql += " AND size = ? ";
             params.push(arrayParams.size);
         }
 
-        if(arrayParams.content){
+        if (arrayParams.content) {
 
             let placeholders = arrayParams.content.map(() => '?').join(', ');
             sql += ` AND item_name IN (${placeholders})
                     GROUP BY bagid
                     HAVING count(*)>= ? `;
             params.push(...arrayParams.content, arrayParams.content.length);
-        }   
-
-       //Rivedere content
-       db.all(sql, params, (err,rows)=>{
-        if(err)
-            reject(err);
-        else{
-            //TODO how to manipulate the results
-            const result = rows.map((row)=>console.log(row));
-            resolve(result);
         }
-       })
 
-
+        //Rivedere content
+        db.all(sql, params, (err, rows) => {
+            if (err)
+                reject(err);
+            else {
+                //TODO how to manipulate the results
+                const result = rows.map((row) => console.log(row));
+                resolve(result);
+            }
+        })
 
     })
-
-/*
-    //filter by  resturant/store by the name
-    this.findName = (name) => this.rs.find((p) => p.name == name);
-    //filter by the bag  price range
-    this.findByPriceRange = (userPrice) => this.rs.flatMap((res) => res.bags).filter((bag) => bag.price <= userPrice);
-    //filter by the bags size
-    this.findBySize = (userSize) => this.rs.flatMap((res) => res.bags).filter((bag) => bag.size == userSize);
-    //filter by the bags type
-    this.findByType = (userType) => this.rs.flatMap((res) => res.bags).filter((bag) => bag.type == userType);
-    //filter by the content of the bag, parameter reset
-    this.findByContet = (...userContent) => this.rs.flatMap((res) => res.bags).find((bag) => userContent.every(element => bag.content.map((x) => x.name).includes(element)));
-*/
 }
-
-
-
 
